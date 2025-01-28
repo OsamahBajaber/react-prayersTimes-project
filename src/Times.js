cc0fd5f8895c3c4d1c4d1858099073633eb0bfe0
Test <test@Abdullahs-MacBook-Pro-2.local>
2025-01-28 15:00:40 +0300

    reducing cards and deploying TimesList to call and send Times

--- src/Times.js
+++ src/Times.js
@@ -1,12 +1,11 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Stack } from "@mui/material";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchAthan } from "./athanSlice";
 
-export default function Times() {
+export default function Times({ name }) {
   const dispatch = useDispatch();
   const { city, prayers } = useSelector((state) => state.athanApi);
   useEffect(() => {
@@ -15,236 +14,41 @@ export default function Times() {
 
   return (
     <>
-      <Stack
-        direction="row"
-        sx={{ margin: "5rem 3rem", backgroundColor: "transparent" }}
-        justifyContent="space-between"
+      <Card
+        sx={{
+          width: "15%",
+          height: "100px",
+        }}
       >
-        <Card
-          sx={{
-            width: "15%",
-            height: "100px",
+        <div
+          style={{
+            width: "100%",
+            height: "30%",
+            display: "flex",
+            justifyContent: "center",
+            // alignItems: "center",
+            background: "linear-gradient(to right, #472dbc,rgb(118, 91, 238))",
           }}
         >
-          <div
-            style={{
-              width: "100%",
-              height: "30%",
-              display: "flex",
-              justifyContent: "center",
-              // alignItems: "center",
-              background:
-                "linear-gradient(to right, #472dbc,rgb(118, 91, 238))",
-            }}
-          >
-            <h4 className="prayer-name">الفجر</h4>
-          </div>
-          <CardContent
-            style={{
-              position: "relative",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography
-              id="Fajr"
-              variant="p"
-              style={{ position: "absolute", top: "50%" }}
-            >
-              {prayers.fajer}
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <Card
-          sx={{
-            width: "15%",
-            height: "100px",
+          <h4 className="prayer-name">{name}</h4>
+        </div>
+        <CardContent
+          style={{
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <div
-            style={{
-              width: "100%",
-              height: "30%",
-              display: "flex",
-              justifyContent: "center",
-              // alignItems: "center",
-              background:
-                "linear-gradient(to right, #472dbc,rgb(118, 91, 238))",
-            }}
+          <Typography
+            id="Fajr"
+            variant="p"
+            style={{ position: "absolute", top: "50%" }}
           >
-            <h4 className="prayer-name">الشروق</h4>
-          </div>
-
-          <CardContent
-            style={{
-              position: "relative",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography
-              id="Sunrise"
-              variant="p"
-              style={{ position: "absolute", top: "50%" }}
-            >
-              {prayers.sunrise}
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <Card
-          sx={{
-            width: "15%",
-            height: "100px",
-          }}
-        >
-          <div
-            style={{
-              width: "100%",
-              height: "30%",
-              display: "flex",
-              justifyContent: "center",
-              // alignItems: "center",
-              background:
-                "linear-gradient(to right, #472dbc,rgb(118, 91, 238))",
-            }}
-          >
-            <h4 className="prayer-name">الظهر</h4>
-          </div>
-
-          <CardContent
-            style={{
-              position: "relative",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography
-              id="Dhuhr"
-              variant="p"
-              style={{ position: "absolute", top: "50%" }}
-            >
-              {prayers.dhuhr}
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <Card
-          sx={{
-            width: "15%",
-            height: "100px",
-          }}
-        >
-          <div
-            style={{
-              width: "100%",
-              height: "30%",
-              display: "flex",
-              justifyContent: "center",
-              // alignItems: "center",
-              background:
-                "linear-gradient(to right, #472dbc,rgb(118, 91, 238))",
-            }}
-          >
-            <h4 className="prayer-name">العصر</h4>
-          </div>
-
-          <CardContent
-            style={{
-              position: "relative",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography
-              id="Asr"
-              variant="p"
-              style={{ position: "absolute", top: "50%" }}
-            >
-              {prayers.asr}
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <Card
-          sx={{
-            width: "15%",
-            height: "100px",
-          }}
-        >
-          <div
-            style={{
-              width: "100%",
-              height: "30%",
-              display: "flex",
-              justifyContent: "center",
-              // alignItems: "center",
-              background:
-                "linear-gradient(to right, #472dbc,rgb(118, 91, 238))",
-            }}
-          >
-            <h4 className="prayer-name">المغرب</h4>
-          </div>
-          <CardContent
-            style={{
-              position: "relative",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography
-              id="Maghrib"
-              variant="p"
-              style={{ position: "absolute", top: "50%" }}
-            >
-              {prayers.maghrib}
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <Card
-          sx={{
-            width: "15%",
-            height: "100px",
-          }}
-        >
-          <div
-            style={{
-              width: "100%",
-              height: "30%",
-              display: "flex",
-              justifyContent: "center",
-              // alignItems: "center",
-              background:
-                "linear-gradient(to right, #472dbc,rgb(118, 91, 238))",
-            }}
-          >
-            <h4 className="prayer-name">العشاء</h4>
-          </div>
-          <CardContent
-            style={{
-              position: "relative",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography
-              id="Isha"
-              variant="p"
-              style={{ position: "absolute", top: "50%" }}
-            >
-              {prayers.isha}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Stack>
+            {prayers[name]}
+          </Typography>
+        </CardContent>
+      </Card>
     </>
   );
 }
--- src/TimesList.js
+++ src/TimesList.js
@@ -1,5 +1,21 @@
 import Times from "./Times";
+import { Stack } from "@mui/material";
 
 export default function TimesList() {
-  return <Times />;
+  return (
+    <>
+      <Stack
+        direction="row"
+        sx={{ margin: "5rem 3rem", backgroundColor: "transparent" }}
+        justifyContent="space-between"
+      >
+        <Times name="fajer" />
+        <Times name="sunrise" />
+        <Times name="dhuhr" />
+        <Times name="asr" />
+        <Times name="maghrib" />
+        <Times name="isha" />
+      </Stack>
+    </>
+  );
 }
